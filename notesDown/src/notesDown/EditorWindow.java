4d7449692c870d9493abcd39e049bea2fb76a97d
Illisha Singh <36201994+illisha@users.noreply.github.com>
2019-11-25 01:12:34 +0530

    Update EditorWindow.java

--- notesDown/src/notesDown/EditorWindow.java
+++ notesDown/src/notesDown/EditorWindow.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 import java.io.*;
 import java.util.Timer;
+import javax.swing.undo.*;
 
 public class EditorWindow extends JFrame implements ActionListener{
 	private EditorPane editorPane;
@@ -14,10 +15,15 @@ public class EditorWindow extends JFrame implements ActionListener{
 	private FileFactory fileFactory;
 	private File documentFile;
 	private File outputFile;
-	
+	private UndoManager undoMgr__; //Manages the undo operation
+
+    enum UndoActionType {
+
+        UNDO, REDO
+    };
 	public EditorWindow() {
 		setLayout(new BorderLayout());
-
+		 undoMgr__ = new UndoManager();
 		JPanel documentPanel = new JPanel(new GridLayout(1,2));
 		editorPane = new EditorPane();
 		fileFactory = new FileFactory(editorPane.getDocument(), editorPane.getEditorKit());
@@ -28,13 +34,20 @@ public class EditorWindow extends JFrame implements ActionListener{
 		
 		JButton[] buttons = {new JButton("Open"), new JButton("Save"), new JButton("Save As"), 
 				new JButton("Export"), new JButton("Export As")};
+		
+		JButton undoButton = new JButton("Undo");
+        	undoButton.addActionListener(new UndoActionListener(UndoActionType.UNDO));//implements undo action
+        	JButton redoButton = new JButton("Redo");
+        	redoButton.addActionListener(new UndoActionListener(UndoActionType.REDO)); //implements redo action
+
 		JPanel buttonPanel = new JPanel();
 		add(buttonPanel, BorderLayout.NORTH);
 		for (JButton button: buttons) {
 			button.addActionListener(this);
 			buttonPanel.add(button);
 		}			
-		
+		buttonPanel.add(undoButton);
+		buttonPanel.add(redoButton);
 		Timer timer = new Timer();
 		UpdateTask updateTask = new UpdateTask(previewPane);
 		timer.scheduleAtFixedRate(updateTask, 100, 50);
